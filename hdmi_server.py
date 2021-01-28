441fb74413c8bb075462d528fd11b7f1874b67ed
Christian Vollmer <chr2202@gmail.com>
2021-01-28 17:45:34 +0100

    On/Off läuft

--- hdmi_server.py
+++ hdmi_server.py
@@ -1,5 +1,7 @@
+import argparse
 from flask import Flask
 import os
+import logging
 app = Flask(__name__)
 
 
@@ -33,5 +35,44 @@ def hdmiStatus():
     tmp = "HDMI Status" + hdmiStatus
     return tmp
 
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--logfile', required=False,
+                        dest='logfile', help='Name of log file.', type=str, default='server.log')
+    parser.add_argument('-a', '--ip', required=False,
+                        dest='address', help='IP address to host on.', type=str, default='0.0.0.0')
+    parser.add_argument('-p', '--port', required=False,
+                        dest='port', help='Port to host on.', type=int, default=2202)
+    parser.add_argument(
+        '-d', '--debug',
+        help="Print lots of debugging statements",
+        action="store_const", dest="loglevel", const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+
+def print_info(message: str):
+    print(message)
+    logging.info(message)
+
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    
+    # configuration
+    args = parse_args()
+
+    logging.basicConfig(
+        filename=args.logfile, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=args.loglevel)
+    
+    # main process
+    print_info(f'hosting on http://{args.address}:{args.port}')
+    app.run(host=args.address, port=args.port)
+
+
